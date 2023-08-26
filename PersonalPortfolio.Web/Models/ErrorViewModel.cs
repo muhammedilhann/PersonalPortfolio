4ca97a85a4ae41b2d53bebe23aa5ff4cfbb5582a
Muhammed <m.muhammedilhan@gmail.com>
2023-08-26 12:01:26 +0300

    Add project files.

---
+++ PersonalPortfolio.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PersonalPortfolio.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
